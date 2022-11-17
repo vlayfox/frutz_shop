1ef1b3bf3f1230b17a1ffb88c6c9ee43015b4673
vlayfox <117673133+vlayfox@users.noreply.github.com>
2022-11-17 23:43:02 +0300

    blog part 1

---
+++ apps/blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class BlogCategory(models.Model):
+    name = models.CharField(verbose_name='имя категории', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория блога'
+        verbose_name_plural = 'категория блога'
+
+
+class Article(models.Model):
+    category = models.ForeignKey(to=BlogCategory,verbose_name='категория', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Заголовок', max_length=255)
+    text_preview = models.TextField(verbose_name='Текст-ревью', null=True, blank=True)
+    text = models.TextField(verbose_name='Текст')
+    publish_date = models.DateTimeField(verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
