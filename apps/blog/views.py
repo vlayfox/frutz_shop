88f2659138f539056855aa01fb64568d41511fa5
vlayfox <117673133+vlayfox@users.noreply.github.com>
2022-12-16 16:21:45 +0300

    Static_and_bootstrap

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,21 @@
+from apps.blog.models import BlogCategory, Article
 from django.shortcuts import render
 
-# Create your views here.
+
+def blog_category_list(request):
+    blog_categories = BlogCategory.objects.all()
+    return render(request, 'blog/category_list.html', {"categories": blog_categories})
+
+
+def aticle_list(request, category_id):
+    articles = Article.objects.filter(category_id=category_id)
+    category = BlogCategory.objects.get(id=category_id)
+    return render(request, 'blog/article_list.html', {'articles': articles, 'category': category})
+
+
+def article_view(request, category_id, article_id):
+    category = BlogCategory.objects.get(id=category_id)
+    article = Article.objects.get(id=article_id)
+    return render(request, 'blog/article_view.html', {"category": category, "article": article})
+
+
