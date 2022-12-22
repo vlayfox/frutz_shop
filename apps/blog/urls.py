a3605481b7a6764e1cf4cb49516508c298bc703f
vlayfox <117673133+vlayfox@users.noreply.github.com>
2022-12-22 22:54:42 +0300

    tag_found

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from apps.blog.views import blog_category_list, aticle_list,article_view
+from apps.blog.views import blog_category_list, aticle_list, article_view, found_tag_view
 
 urlpatterns = [
     path('', blog_category_list, name='blog_category_list'),
     path('<int:category_id>/', aticle_list, name='blog_article_list'),
-    path('<int:category_id>/<int:article_id>/', article_view, name='blog_article_view')
+    path('tag/<int:tag_id>/', found_tag_view, name='found_tag_view'),
+    path('<int:category_id>/<int:article_id>/', article_view, name='blog_article_view'),
+
 ]
