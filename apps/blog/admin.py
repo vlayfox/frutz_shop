4362100a1826edf04b135d45b1c3abc5a4c9d424
vlayfox <117673133+vlayfox@users.noreply.github.com>
2023-02-17 00:56:13 +0300

    admin trcks

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -10,19 +10,22 @@ admin.site.register(Tag)
 
 @admin.register(BlogCategory)
 class BlogCategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name','image_tag_thumbnail','article_count' ]
-    list_display_links = ['id', 'name','image_tag_thumbnail']
-    fields = ['name','image_tag','image']
+    list_display = ['id', 'name', 'image_tag_thumbnail', 'article_count']
+    list_display_links = ['id', 'name', 'image_tag_thumbnail']
+    fields = ['name', 'image_tag', 'image']
     readonly_fields = ['image_tag']
+
     def article_count(self, instance):
         articles = Article.objects.filter(category=instance).count()
-        url = reverse('admin:blog_article_changelist')+'?'+urlencode({'category__id__exact': instance.id})
+        url = reverse('admin:blog_article_changelist') + '?' + urlencode({'category__id__exact': instance.id})
         return format_html(f"<a  href='{url}'>Статей:{articles}<a/>")
 
-    article_count.short_description='К-ство статей'
+    article_count.short_description = 'К-ство статей'
+
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category_link', 'publish_date', 'created_at']
+    list_display = ['id', 'title', 'category_link','tag_links', 'publish_date', 'created_at']
     list_display_links = ['id', 'title']
     list_filter = ['category', 'tags']
 
@@ -31,3 +34,14 @@ class ArticleAdmin(admin.ModelAdmin):
         return format_html(f"<a href='{url} '   >{instance.category.name}</a>")
 
     category_link.short_description = 'Категории'
+    def tag_links(self, instance):
+        tags = instance.tags.all()
+        data = []
+        for tag in tags:
+            url = reverse('admin:blog_tag_change', args=[tag.id])
+            data.append(f"<a href ={url} >{tag.name}</a>")
+        result=',' .join(data)
+        return format_html(result)
+
+
+    tag_links.short_description = 'Теги'
