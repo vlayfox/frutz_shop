04de3dcdf802d16c72293aff10e138180924905c
vlayfox <117673133+vlayfox@users.noreply.github.com>
2023-02-19 14:34:15 +0300

    product_view

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
-from apps.catalog.models import Category,Product
+from apps.catalog.models import Category, Product, ProductImage
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
 
+
+class ProductCategoryInline(admin.TabularInline):
+    model = Product.categories.through
+    extra = 1
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    fields = ['product', 'image_tag', 'image', 'is_main']
+    readonly_fields = ['image_tag']
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
-#yy
+    inlines = [ProductCategoryInline, ProductImageInline]
+    list_display = ['id', 'image_tag', 'name', 'price', 'created_at']
+    list_display_links = ['id', 'name', 'image_tag']
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views import generic
+from apps.catalog.models import Category, Product
 
-# Create your views here.
+
+class CategoryIndexView(generic.ListView):
+    model = Category
+    template_name = 'catalog/index.html'
+    queryset = Category.objects.filter(parent=None)
+
+
+class ProductsByCategoryView(generic.ListView):
+    template_name = 'catalog/category.html'
+    category = None
+    categories = Category.objects.all()
+
+    def get_queryset(self):
+        self.category = Category.objects.get(slug=self.kwargs['slug'])
+        queryset = Product.objects.filter(categories=self.category)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductsByCategoryView, self).get_context_data(**kwargs)
+        context['category'] = self.category
+        context['categories'] = self.categories
+        return context
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'catalog/product.html'
