290024dc35ffd243a1ddacc78f300c54aaa74174
vlayfox <117673133+vlayfox@users.noreply.github.com>
2023-02-28 02:59:25 +0300

    HT28

---
+++ apps/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,include
+
+
+urlpatterns = [
+    path('user/', include('apps.api.user.urls') ),
+    path('catalog/', include('apps.api.catalog.urls') ),
+    path('blog/', include('apps.api.blog.urls') ),
+
+]
