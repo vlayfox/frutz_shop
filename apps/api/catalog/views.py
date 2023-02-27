e83b7d3497ac276cc6213c681c08a6d5c786d9fd
vlayfox <117673133+vlayfox@users.noreply.github.com>
2023-02-28 02:13:15 +0300

    hometask 27

---
+++ apps/api/catalog/views.py
@@ -0,0 +1,35 @@
+from rest_framework import generics, permissions, viewsets
+from apps.catalog.models import Category, Product, ProductImage
+from apps.api.catalog.serializers import CategorySerializer, ProductReadSerializer, ProductWriteSerializer
+
+
+class ProductListView(generics.ListAPIView):
+    serializer_class = ProductReadSerializer
+    queryset = Product.objects.all()
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    serializer_class = ProductReadSerializer
+    queryset = Product.objects.all()
+
+
+class ProductCreateView(generics.CreateAPIView):
+    serializer_class = ProductWriteSerializer
+    queryset = Product.objects.all()
+    permission_classes = [permissions.IsAdminUser]
+
+
+class ProductDeleteView(generics.CreateAPIView):
+    serializer_class = ProductWriteSerializer
+    queryset = Product.objects.all()
+    permission_classes = [permissions.IsAdminUser]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['create','update','destroy']:
+            return [permission() for permission in [permissions.IsAdminUser]]
+        return [permission() for permission in [permissions.AllowAny]]
